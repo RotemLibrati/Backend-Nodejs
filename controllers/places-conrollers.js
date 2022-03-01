8e65a1fab7d0b6a239dc71c3080f2ca8178950c1
RotemLibrati <rotemlibrati1@gmail.com>
2022-03-01 12:35:39 +0200

    Added delete and update place

--- controllers/places-conrollers.js
+++ controllers/places-conrollers.js
@@ -1,7 +1,7 @@
 const {v4 : uuid} = require('uuid');
 const HttpError = require('../models/http-error');
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
     {
         id: 'p1',
         title: 'Empire State Building',
@@ -46,7 +46,7 @@ const createdPlace = (req, res, next) => {
     const createdPlace = {
         id: uuid(),
         title: title, // same like - title,
-        description,
+        description, 
         location: coordinates,
         address, 
         creator
@@ -56,6 +56,28 @@ const createdPlace = (req, res, next) => {
     res.status(201).json({place: createdPlace});
 };
 
+const updatePlace = (req, res, next) => {
+    const { title, description } = req.body;
+    const placeId = req.params.pid;
+
+    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+    updatedPlace.title= title;
+    updatedPlace.description = description;
+
+    DUMMY_PLACES[placeIndex] = updatedPlace;
+    
+    res.status(200).json({place: updatedPlace});
+};
+
+const deletePlace = (req, res, next) => {
+    placeId = req.params.pid;
+    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+    res.status(200).json({message: "Deleted place "});
+}; 
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createdPlace = createdPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
